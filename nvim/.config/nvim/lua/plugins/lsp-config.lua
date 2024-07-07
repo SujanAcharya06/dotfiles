b156ed29cd38d1de320aaea52845aa96e92b51fc
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-07-08 00:28:12 +0530

    Updated lsp-config.lua to get autocomplete

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,8 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "pylsp" }
+        -- ensure_installed = { "lua_ls", "clangd", "pylsp" }
+        auto_install = true,
       })
     end
   },
@@ -17,19 +18,31 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
 
       -- for lua
-      lspconfig.lua_ls.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
       -- Setup for clangd
       lspconfig.clangd.setup({
+        capabilities = capabilities,
         filetypes = { "c", "objc",} -- {"cpp", "objcpp"}
       })
 
       -- Setup for pylsp
-      lspconfig.pylsp.setup({})
+      lspconfig.pylsp.setup({
+        capabilities = capabilities,
+      })
+
+      --Setup for html
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
