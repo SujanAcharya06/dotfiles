d4f1b6c2bea53892a5ec17880b651e367b5f5c36
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-07-07 12:57:26 +0530

    Added treesitter plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,7 +2,12 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+-- sets the padding from start of the number
+-- vim.cmd("set number")
+vim.cmd("set numberwidth=4")
+vim.cmd("set relativenumber")
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -27,19 +32,26 @@ local plugins = {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     -- or                              , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+-- Telescope configs
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
+-- Treesitter configs
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "cpp", "javascript"},
+--  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },  
+})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
-
-
-
