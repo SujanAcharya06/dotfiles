bbf8e19a919b104c3a6c6e210e07ad27bff6c646
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-07-08 12:46:35 +0530

    added jdtls lsp server

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "pyright" }
+        ensure_installed = { "lua_ls", "clangd", "pyright", "jdtls" }
         -- auto_install = true,
       })
     end
@@ -71,6 +71,11 @@ return {
 
       -- Setup for html
       lspconfig.html.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      lspconfig.jdtls.setup({
         capabilities = capabilities,
         on_attach = on_attach,
       })
