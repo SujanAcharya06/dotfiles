b9974d591fcbbff5e01b823fee69fdc664e12c93
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-07-08 11:52:52 +0530

    Added complete configuaration file for lsp config current lsp-configs - lua_ls - pyright - html - clangd

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,8 +9,8 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        -- ensure_installed = { "lua_ls", "clangd", "pylsp" }
-        auto_install = true,
+        ensure_installed = { "lua_ls", "clangd", "pyright" }
+        -- auto_install = true,
       })
     end
   },
@@ -23,25 +23,56 @@ return {
 
       local lspconfig = require("lspconfig")
 
+      -- Define the on_attach function
+      local on_attach = function(client, bufnr)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        -- Mappings.
+        local opts = { noremap=true, silent=true, buffer=bufnr }
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wl', function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+      end
+
       -- for lua
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
+        on_attach = on_attach,
       })
 
       -- Setup for clangd
       lspconfig.clangd.setup({
         capabilities = capabilities,
+        on_attach = on_attach,
         filetypes = { "c", "objc",} -- {"cpp", "objcpp"}
       })
 
-      -- Setup for pylsp
-      lspconfig.pylsp.setup({
+      -- Setup for pyright
+      lspconfig.pyright.setup({
         capabilities = capabilities,
+        on_attach = on_attach,
       })
 
-      --Setup for html
+      -- Setup for html
       lspconfig.html.setup({
         capabilities = capabilities,
+        on_attach = on_attach,
       })
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
@@ -51,3 +82,4 @@ return {
     end
   }
 }
+
