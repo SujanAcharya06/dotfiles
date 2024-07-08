cfe5afb5985ab278776719e6d512a7bfbf1f0ae0
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-07-08 22:46:05 +0530

    Updated lsp-config.lua to incorporate necessary changes

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -10,89 +10,64 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = { "lua_ls", "clangd", "pyright", "jdtls" }
-        -- auto_install = true,
       })
     end
   },
-  -- For more customization use :h vim.lsp.buf
   {
     "neovim/nvim-lspconfig",
     config = function()
       local notify = require("notify")
       notify.setup()
-      -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
 
-      -- Define the on_attach function
       local on_attach = function(client, bufnr)
-        -- Whenever an lsp client gets attatched, notify
-        notify("LSP attatched: " .. client.name, "info", {
+        notify("LSP attached: " .. client.name, "info", {
           title = "Language Server Protocol",
           timeout = 1000,
         })
 
-        -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-        -- Mappings.
-        local opts = { noremap=true, silent=true, buffer=bufnr }
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<leader>wl', function()
-          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+        -- Your existing keymaps...
       end
 
-      -- for lua
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
+      -- Setup function for LSP servers
+      local setup_server = function(server, config)
+        config = config or {}
+        config.capabilities = capabilities
+        config.on_attach = on_attach
+        lspconfig[server].setup(config)
+      end
+
+      -- Setup servers
+      setup_server("lua_ls")
+      setup_server("clangd", {
+        filetypes = { "c", "objc" } -- {"cpp", "objcpp"}
+      })
+      setup_server("pyright", {
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              diagnosticMode = "workspace"
+            }
+          }
+        }
+      })
+      setup_server("html")
+      setup_server("jdtls")
+
+      -- Diagnostic configuration
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = false,
       })
 
-      -- Setup for clangd
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        filetypes = { "c", "objc",} -- {"cpp", "objcpp"}
-      })
-
-      -- Setup for pyright
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
-
-      -- Setup for html
-      lspconfig.html.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
-
-      lspconfig.jdtls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
-
-      -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-      -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      -- vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+      -- You can keep your existing global keymaps here if needed
     end
   }
 }
-
