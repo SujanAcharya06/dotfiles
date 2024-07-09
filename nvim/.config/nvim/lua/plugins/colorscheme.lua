10d62515c8c872cd29628168a4a2fa961dcd289a
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-07-09 19:22:25 +0530

    Configured nord

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,36 +1,29 @@
-return {
-  -- {
-  --   'marko-cerovac/material.nvim',
-  --   config = function()
-  --     --Lua:
-  --     vim.g.material_style = "deep ocean" -- {"deep ocean", "darker", "lighter"}
-  --     --Lua:
-  --     vim.cmd 'colorscheme material'
-  --   end,
-  -- },
+return{
+  {
+    'marko-cerovac/material.nvim',
+    config = function()
+    --   vim.g.material_style = "deep ocean"
+    --   vim.cmd('colorscheme material')
+    end,
+  },
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
     config = function()
-      -- vim.cmd.colorscheme("catppuccin")
+    --   vim.cmd.colorscheme("catppuccin")
     end,
   },
   {
     "shaunsingh/nord.nvim",
     config = function()
-      --Lua:
-      vim.cmd[[colorscheme nord]]
-      -- -- Example config in lua
       vim.g.nord_contrast = true
-      vim.g.nord_borders = true 
+      vim.g.nord_borders = true
       vim.g.nord_disable_background = false
       vim.g.nord_italic = false
       vim.g.nord_uniform_diff_background = true
       vim.g.nord_bold = false
-
-      -- Load the colorscheme
       require('nord').set()
-    end,
+    end
   }
 }
