1c869f363ffebaf99e4c4afbcef1b7170e3c84fd
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-07-09 18:10:53 +0530

    Merged two single file - colorscheme.lua

--- nvim/.config/nvim/lua/plugins/material.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'marko-cerovac/material.nvim',
-  config = function()
-    --Lua:
-    vim.g.material_style = "deep ocean" -- {"deep ocean", "darker", "lighter"}
-    --Lua:
-    vim.cmd 'colorscheme material'
-  end,
-}
