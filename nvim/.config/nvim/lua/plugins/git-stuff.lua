313e8a2e58e0a6fb794b6cc03ade181bf19ec29c
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-07-09 12:43:59 +0530

    Added new git configs to neovim

---
+++ nvim/.config/nvim/lua/plugins/git-stuff.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "tpope/vim-fugitive",
+  },
+  { "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup({
+        signs = {
+          add          = { text = '+' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        signs_staged = {
+          add          = { text = '┃' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        signs_staged_enable = true,
+      })
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+    end,
+  }
+}
+
