7dd790bbaf8867a2c81dfd128e8d599b62ae2833
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-07-10 14:01:08 +0530

    Update completions.lua to configure omni function

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -2,7 +2,6 @@ return {
   {
     "hrsh7th/cmp-nvim-lsp"
   },
-
   {
     "L3MON4D3/LuaSnip",
     dependencies = {
@@ -10,26 +9,19 @@ return {
       "rafamadriz/friendly-snippets",
     },
   },
-
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      "hrsh7th/cmp-omni",  -- Add this line to include cmp-omni
+      "hrsh7th/cmp-omni",
     },
     config = function()
       -- Set up nvim-cmp.
       local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
-
       cmp.setup({
         snippet = {
-          -- REQUIRED - you must specify a snippet engine
           expand = function(args)
-            -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
         window = {
@@ -41,15 +33,12 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          -- { name = "vsnip" }, -- For vsnip users.
-          { name = "luasnip" }, -- For luasnip users.
+          { name = "luasnip" },
           { name = "omni" },
-          -- { name = 'ultisnips' }, -- For ultisnips users.
-          -- { name = 'snippy' }, -- For snippy users.
         }, {
           { name = "buffer" },
         }),
