2e061fdd997a4b3b3824846b31beb0cdade7c852
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-07-10 15:56:15 +0530

    Added key map to navigate and close throurgh bufferline

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -4,6 +4,12 @@ return{
     dependencies = {'nvim-tree/nvim-web-devicons'},
     config = function()
       require("bufferline").setup{}
+      vim.keymap.set({'n'}, '<space>xx', ':BufferLineCloseOthers<CR>',{})
+      vim.keymap.set({'n'}, '<C-h>', ':BufferLineCyclePrev<CR>',{})
+      vim.keymap.set({'n'}, '<C-l>', ':BufferLineCycleNext<CR>',{})
+      vim.keymap.set({'n'}, '<space>hx', ':BufferLineCloseLeft<CR>',{})
+      vim.keymap.set({'n'}, '<space>hx', ':BufferLineCloseLeft<CR>',{})
+      vim.keymap.set({'n'}, '<space>lx', ':BufferLineCloseRight<CR>',{})
     end,
   }
 }
