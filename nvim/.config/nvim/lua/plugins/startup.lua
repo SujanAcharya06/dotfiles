0e9d6c646486b45b31621e0e17a16afc5bb9a951
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-07-19 22:32:45 +0530

    Reverted back to alpha.lua

--- nvim/.config/nvim/lua/plugins/startup.lua
+++ nvim/.config/nvim/lua/plugins/startup.lua
@@ -1,47 +1,47 @@
 return {
-    {
-        "startup-nvim/startup.nvim",
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-            "nvim-lua/plenary.nvim"
-        },
-        -- themes
-        -- dashboard (default)
-        -- evil
-        -- startify
-        config = function()
-            require("startup").setup({theme = "dashboard"})
-        end
-    }
+    -- {
+    --     "startup-nvim/startup.nvim",
+    --     dependencies = {
+    --         "nvim-telescope/telescope.nvim",
+    --         "nvim-lua/plenary.nvim"
+    --     },
+    --     -- themes
+    --     -- dashboard (default)
+    --     -- evil
+    --     -- startify
+    --     config = function()
+    --         require("startup").setup({theme = "dashboard"})
+    --     end
+    -- },
+    -- {
+
+    "goolord/alpha-nvim",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.startify")
+
+        dashboard.section.header.val = {
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                     ]],
+            [[       ████ ██████           █████      ██                     ]],
+            [[      ███████████             █████                             ]],
+            [[      █████████ ███████████████████ ███   ███████████   ]],
+            [[     █████████  ███    █████████████ █████ ██████████████   ]],
+            [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+            [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+            [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+        }
+
+        alpha.setup(dashboard.opts)
+    end,
 }
--- return {
---   "goolord/alpha-nvim",
---   dependencies = {
---     "nvim-tree/nvim-web-devicons",
---   },
---
---   config = function()
---     local alpha = require("alpha")
---     local dashboard = require("alpha.themes.startify")
---
---     dashboard.section.header.val = {
---       [[                                                                       ]],
---       [[                                                                       ]],
---       [[                                                                       ]],
---       [[                                                                       ]],
---       [[                                                                     ]],
---       [[       ████ ██████           █████      ██                     ]],
---       [[      ███████████             █████                             ]],
---       [[      █████████ ███████████████████ ███   ███████████   ]],
---       [[     █████████  ███    █████████████ █████ ██████████████   ]],
---       [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
---       [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
---       [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
---       [[                                                                       ]],
---       [[                                                                       ]],
---       [[                                                                       ]],
---     }
---
---     alpha.setup(dashboard.opts)
---   end,
--- }
