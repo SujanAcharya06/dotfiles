af4d647addd6d1c50df807dea7d7b8223b79993f
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-08-02 16:06:05 +0530

    Changed key maps for resizeing panes vertically

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -27,6 +27,6 @@ vim.api.nvim_set_keymap('n', '<leader>]', ':Lazy<CR>', {noremap = true})
 -- Resize splits using Alt key
 -- vim.api.nvim_set_keymap('n', '<A-h>', ':resize +2<CR>', { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap('n', '<A-l>', ':resize -2<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-h>', ':vertical resize -2<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<A-l>', ':vertical resize +2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-,>', ':vertical resize -2<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-.>', ':vertical resize +2<CR>', { noremap = true, silent = true })
 
