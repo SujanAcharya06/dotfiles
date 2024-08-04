8055dab57ea0bcddf1589ce2ca6028997b38721d
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-08-04 18:47:22 +0530

    Modified autopairs code

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -1,33 +1,32 @@
 return {
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = function()
-      local npairs = require("nvim-autopairs")
-      local Rule = require('nvim-autopairs.rule')
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = function()
+            local npairs = require("nvim-autopairs")
+            local Rule = require('nvim-autopairs.rule')
+            npairs.setup({})
 
-      npairs.setup({})
+            -- Variable to track the state of autopairs
+            _G.autopairs_enabled = true
 
-      -- Variable to track the state of autopairs
-      local autopairs_enabled = true
+            -- Function to toggle autopairs
+            _G.toggle_autopairs = function()
+                _G.autopairs_enabled = not _G.autopairs_enabled
+                if _G.autopairs_enabled then
+                    npairs.enable()
+                    print("Autopairs enabled")
+                else
+                    npairs.disable()
+                    print("Autopairs disabled")
+                end
+            end
 
-      -- Function to toggle autopairs
-      local function toggle_autopairs()
-        autopairs_enabled = not autopairs_enabled
-        if autopairs_enabled then
-          npairs.enable()
-          print("Autopairs enabled")
-        else
-          npairs.disable()
-          print("Autopairs disabled")
+            -- Set up the keybinding to toggle autopairs
+            vim.api.nvim_set_keymap('n', '<C-i>', ':lua toggle_autopairs()<CR>', {noremap = true, silent = true})
+
+            -- Override the toggle function of nvim-autopairs
+            npairs.toggle = _G.toggle_autopairs
         end
-      end
-
-      -- Set up the keybinding to toggle autopairs
-      vim.api.nvim_set_keymap('n', '<C-i>', ':lua require("nvim-autopairs").toggle()<CR>', {noremap = true, silent = true})
-
-      -- Override the toggle function of nvim-autopairs
-      npairs.toggle = toggle_autopairs
-    end
-  }
+    }
 }
