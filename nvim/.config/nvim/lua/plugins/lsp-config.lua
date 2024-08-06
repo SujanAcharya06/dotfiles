5726fb228b4ffb02b357d60b5abd9b81b65126f9
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-08-06 22:21:20 +0530

    Added lsp for html, cssls, and jssserver

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,19 +9,28 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd", "jdtls", "pyright", "html"}
+                ensure_installed = { "lua_ls", "clangd", "jdtls", "pyright", "html", "cssls", "tsserver" }
             })
         end
     },
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "onsails/lspkind.nvim",
+        },
         config = function()
             local notify = require("notify")
             notify.setup({
                 top_down = false
             })
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            -- Add snippet support to capabilities
             capabilities.textDocument.completion.completionItem.snippetSupport = true
             local lspconfig = require("lspconfig")
             local on_attach = function(client, bufnr)
@@ -49,6 +58,22 @@ return {
                 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
                 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
                 vim.keymap.set({'n', 'v'}, '<space>ca', vim.lsp.buf.code_action, opts)
+
+                -- Add this for automatic hover
+                vim.api.nvim_create_autocmd("CursorHold", {
+                    buffer = bufnr,
+                    callback = function()
+                        local opts = {
+                            focusable = false,
+                            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+                            border = 'rounded',
+                            source = 'always',
+                            prefix = ' ',
+                            scope = 'cursor',
+                        }
+                        vim.diagnostic.open_float(nil, opts)
+                    end
+                })
             end
 
             -- Setup function for LSP servers
@@ -62,7 +87,7 @@ return {
                 lspconfig[server].setup(config)
             end
 
-            -- Setup servers
+            -- Setup servers (keep your existing server setups)
             setup_server("lua_ls")
             setup_server("clangd", {
                 cmd = {
@@ -74,7 +99,7 @@ return {
                     "--suggest-missing-includes",
                     "--all-scopes-completion"
                 },
-                filetypes = { "c", "objc"}, -- {"cpp", "objcpp"}
+                filetypes = { "c", "objc" },
                 init_options = {
                     clangdFileStatus = true,
                     usePlaceholders = true,
@@ -95,6 +120,42 @@ return {
             })
             setup_server("html")
             setup_server("jdtls")
+            setup_server("html", {
+                filetypes = { "html" },
+                init_options = {
+                    configurationSection = { "html", "css", "javascript" },
+                    embeddedLanguages = {
+                        css = true,
+                        javascript = true
+                    },
+                    provideFormatter = true
+                }
+            })
+            setup_server("cssls", {
+                filetypes = { "css", "scss", "less" },
+                settings = {
+                    css = {
+                        validate = true
+                    },
+                    less = {
+                        validate = true
+                    },
+                    scss = {
+                        validate = true
+                    }
+                }
+            })
+            setup_server("tsserver", {
+                filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+                cmd = { "typescript-language-server", "--stdio" },
+                init_options = {
+                    hostInfo = "neovim",
+                    preferences = {
+                        includeCompletionsForModuleExports = true,
+                        includeCompletionsForImportStatements = true,
+                    }
+                }
+            })
 
             -- Diagnostic configuration
             vim.diagnostic.config({
@@ -104,7 +165,77 @@ return {
                 update_in_insert = false,
                 severity_sort = false,
             })
-            -- You can keep your existing global keymaps here if needed
+
+            -- Set up nvim-cmp
+            local cmp = require("cmp")
+            local lspkind = require("lspkind")
+            require("luasnip.loaders.from_vscode").lazy_load()
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered({
+                        winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
+                    }),
+                    documentation = cmp.config.window.bordered({
+                        winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
+                    }),
+                },
+                formatting = {
+                    format = lspkind.cmp_format({
+                        mode = "symbol_text",
+                        maxwidth = 50,
+                        ellipsis_char = "...",
+                        before = function(entry, vim_item)
+                            vim_item.menu = ({
+                                nvim_lsp = "[LSP]",
+                                luasnip = "[Snippet]",
+                                buffer = "[Buffer]",
+                                path = "[Path]",
+                            })[entry.source.name]
+                            return vim_item
+                        end
+                    }),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "path" },
+                    { name = "buffer", keyword_length = 5 },
+                }),
+                experimental = {
+                    ghost_text = true,
+                },
+            })
+
+            -- Use buffer source for `/` and `?`
+            cmp.setup.cmdline({ '/', '?' }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+
+            -- Use cmdline & path source for ':'
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    { name = 'cmdline' }
+                })
+            })
         end
     }
 }
