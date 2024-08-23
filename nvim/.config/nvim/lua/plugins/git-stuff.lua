9fe56a484b9abaf2ab59a5433a853e4299c4c8a2
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-08-23 21:31:50 +0530

    Updated git-stuff.lua     - Changed the signs for tracked files     - add, edit and delete for tracked files

--- nvim/.config/nvim/lua/plugins/git-stuff.lua
+++ nvim/.config/nvim/lua/plugins/git-stuff.lua
@@ -29,10 +29,10 @@ return {
         config = function()
             require('gitsigns').setup({
                 signs = {
-                    add          = { text = '▎' },
-                    change       = { text = '▎' },
-                    delete       = { text = '➖' },
-                    topdelete    = { text = '🔺' },
+                    add          = { text = '+' },
+                    change       = { text = '~' },
+                    delete       = { text = '🔺' },
+                    topdelete    = { text = '➖' },
                     changedelete = { text = '◢' },
                     untracked    = { text = '◈' },
                 },
