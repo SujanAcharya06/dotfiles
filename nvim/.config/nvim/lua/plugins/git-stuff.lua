0a77f3023d4814a224e554ef2694c8e2b59b6fb9
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-08-30 14:45:39 +0530

    Updated git-stuff.lua - changed `signs` for `staged` and `unstaged` files. - added additional keymaps from the official repo.

--- nvim/.config/nvim/lua/plugins/git-stuff.lua
+++ nvim/.config/nvim/lua/plugins/git-stuff.lua
@@ -27,27 +27,55 @@ return {
     {
         "lewis6991/gitsigns.nvim",
         config = function()
-            require('gitsigns').setup({
+            local gitsigns = require('gitsigns')
+            gitsigns.setup({
                 signs = {
                     add          = { text = '+' },
-                    change       = { text = '~' },
-                    delete       = { text = '🔺' },
-                    topdelete    = { text = '➖' },
-                    changedelete = { text = '◢' },
-                    untracked    = { text = '◈' },
+                    change       = { text = '^' },
+                    delete       = { text = '_' },
+                    topdelete    = { text = '‾' },
+                    changedelete = { text = '~' },
+                    untracked    = { text = '┆' },
                 },
                 signs_staged = {
-                    add          = { text = '✚' },
-                    change       = { text = '✎' },
-                    delete       = { text = '✖' },
-                    topdelete    = { text = '🔻' },
-                    changedelete = { text = '◣' },
-                    untracked    = { text = '◇' },
+                    add          = { text = '+' },
+                    change       = { text = '^' },
+                    delete       = { text = '_' },
+                    topdelete    = { text = '‾' },
+                    changedelete = { text = '~' },
+                    untracked    = { text = '┆' },
                 },
                 signs_staged_enable = true,
             })
+
+            -- Helper function for mapping keys
+            local function map(mode, lhs, rhs, opts)
+                opts = opts or {}
+                opts.silent = opts.silent ~= false
+                vim.keymap.set(mode, lhs, rhs, opts)
+            end
+
+            -- Existing keymaps
             vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
             vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+
+            -- New keymaps
+            map('n', '<leader>hs', gitsigns.stage_hunk)
+            map('n', '<leader>hr', gitsigns.reset_hunk)
+            map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+            map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+            map('n', '<leader>hS', gitsigns.stage_buffer)
+            map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+            map('n', '<leader>hR', gitsigns.reset_buffer)
+            map('n', '<leader>hp', gitsigns.preview_hunk)
+            map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+            map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+            map('n', '<leader>hd', gitsigns.diffthis)
+            map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+            map('n', '<leader>td', gitsigns.toggle_deleted)
+
+            -- Text object
+            map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
         end,
     }
 }
