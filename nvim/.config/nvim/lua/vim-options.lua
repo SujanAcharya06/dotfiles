09315b9c1bd542fa3dc1b3ae6c8acb321526d769
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-09-04 20:54:42 +0530

    Updated vim-options.lua     - Added tab listchar

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -38,17 +38,18 @@ vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,"..
    "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,"..
     "sm:block-blinkwait175-blinkoff150-blinkon175";
 
+-- Enable listchars
+vim.opt.list = true
 -- Set listchars
 vim.opt.listchars = {
     space = '⋅',
     -- eol = '↴',
     trail = '~',
+    tab = '│ ',
     extends = '›',
     precedes = '‹',
     nbsp = '␣',
 }
--- Enable listchars
-vim.opt.list = true
 
 -- Folding settings
 vim.opt.foldcolumn = "0"
