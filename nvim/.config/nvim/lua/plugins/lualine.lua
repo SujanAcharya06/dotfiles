127e1628eeecfc8d19bc294614897ce91f1eedbd
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-09-16 19:43:12 +0530

    Updated lualine.lua     - Added `onedark` theme

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
     require('lualine').setup({
       options = {
         icons_enables = true;
-        theme = "carbonfox" --{"dracula"}
+        theme = "onedark" --{"dracula"}
       }
     })
   end
