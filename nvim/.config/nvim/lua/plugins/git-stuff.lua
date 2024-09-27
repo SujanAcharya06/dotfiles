1d6dd20b4513fa898788f8cc38a147f70125890b
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-09-27 22:48:44 +0530

    Updated git-stuff.lua     - indented code     - new keymap for :Gvdiffsplit     - changed keymap for force push from `<leader>ggf` -> `<leader>ggp`

--- nvim/.config/nvim/lua/plugins/git-stuff.lua
+++ nvim/.config/nvim/lua/plugins/git-stuff.lua
@@ -1,8 +1,9 @@
 return {
 	{
-		"tpope/vim-fugitive", config = function() local git_window_open = false
+		"tpope/vim-fugitive",
+		config = function()
+			local git_window_open = false
 			local git_window_bufnr = nil
-
 			local function toggle_git_window()
 				if git_window_open then
 					if git_window_bufnr and vim.api.nvim_buf_is_valid(git_window_bufnr) then
@@ -18,10 +19,11 @@ return {
 			end
 
 			vim.keymap.set("n", "<leader>gs", toggle_git_window, {noremap = true, silent = true})
+			vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR>", {noremap = true, silent = true})
 			vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", {noremap = true, silent = true})
 			vim.keymap.set("n", "<leader>P", ":Git push<CR>", {noremap = true, silent = true})
 			vim.keymap.set('n', '<leader>gl', ':vert Git log --oneline<CR>', { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>ggf", ":Git push -f origin main<CR>", {noremap = true, silent = true})
+			vim.keymap.set("n", "<leader>ggp", ":Git push -f origin main<CR>", {noremap = true, silent = true})
 		end
 	},
 	{
