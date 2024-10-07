92c21109cb441251d942162b96de6657d614eeb1
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-10-07 21:21:21 +0530

    Updated .aliases.zsh     - Changed Resource directory location

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -5,8 +5,8 @@ alias l='ls -CF'
 
 alias cls='clear'
 
-alias mail='python /home/sujanacharya/Documents/Resource/Code/Python/Py-Scripts/test.py'
-alias mail-log='python /home/sujanacharya/Documents/Resource/Code/Python/Py-Scripts/test-logfile.py'
+alias mail='python /home/sujanacharya/syncthing/Resource/Code/Python/Py-Scripts/test.py'
+alias mail-log='python /home/sujanacharya/syncthing/Resource/Code/Python/Py-Scripts/test-logfile.py'
 
 alias cat='bat'
 alias ls='exa'
@@ -14,8 +14,8 @@ alias ll='exa -alh'
 alias tree='exa --tree'
 
 # For executing cpp program
-alias exe-cpp='sh /home/sujanacharya/Documents/Resource/Code/Bash_Scripts/bashScripts/Auto-Exec/exe-cpp.sh'
-alias exe-java='sh /home/sujanacharya/Documents/Resource/Code/Bash_Scripts/bashScripts/Auto-Exec/exe-java.sh'
+alias exe-cpp='sh /home/sujanacharya/syncthing/Resource/Code/Bash_Scripts/bashScripts/Auto-Exec/exe-cpp.sh'
+alias exe-java='sh /home/sujanacharya/syncthing/Resource/Code/Bash_Scripts/bashScripts/Auto-Exec/exe-java.sh'
 
 # For storing the cwd of the folder in ranger
 alias ranger='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
@@ -26,7 +26,7 @@ alias rm='trash'
 alias copy='xsel --input --clipboard'
 alias paste='xsel --output --clipboard'
 
-alias fileCount='python /home/sujanacharya/Documents/Resource/Code/Python/Py-Scripts/fileCount.py'
+alias fileCount='python /home/sujanacharya/syncthing/Resource/Code/Python/Py-Scripts/fileCount.py'
 
 # For git fetch
 alias fetch='git fetch origin main'
@@ -43,7 +43,10 @@ alias fcd='cd "$(dirname "$(fzf)")"'
 #For doom emacs
 alias emacs='emacsclient -c -a 'emacs''
 
-#Wifi quick connect 
+#Wifi quick connect
 alias w='nmcli c up $WIFI'
 
 alias nvim='nvm > /dev/null 2>&1 && nvim'
+
+# alias dr='/home/sujanacharya/syncthing/remote-sync/dry-run.sh'
+# alias snd='/home/sujanacharya/syncthing/remote-sync/send.sh'
