40372909811ca948a78d2666a5911416e899e971
bufferOverflow06 <sujanjacharya123@gmail.com>
2024-10-29 21:35:04 +0530

    Updated autopairs.lua     - Added autospace support

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -3,6 +3,8 @@ return {
     event = "InsertEnter",
     opts = {
         ignored_next_char = "[%w%.]",
+        enable_afterquote = true,  -- add bracket pairs after quote
+        enable_check_bracket_line = true,  -- check bracket in same line
         fast_wrap = {
             map = "<C-w>",
             chars = { "{", "[", "(", '"', "'" },
@@ -18,7 +20,35 @@ return {
         local npairs = require("nvim-autopairs")
         npairs.setup(opts)
 
-        local Rule = require("nvim-autopairs.rule")
-        -- npairs.add_rule(Rule("<", ">"))
+        -- Add space rules
+        local Rule = require('nvim-autopairs.rule')
+        local cond = require('nvim-autopairs.conds')
+
+        -- Add spaces between parentheses
+        npairs.add_rules({
+            Rule(' ', ' ')
+                :with_pair(function(opts)
+                    local pair = opts.line:sub(opts.col - 1, opts.col)
+                    return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+                end),
+            Rule('( ', ' )')
+                :with_pair(function() return false end)
+                :with_move(function(opts)
+                    return opts.prev_char:match('.%)') ~= nil
+                end)
+                :use_key(')'),
+            Rule('{ ', ' }')
+                :with_pair(function() return false end)
+                :with_move(function(opts)
+                    return opts.prev_char:match('.%}') ~= nil
+                end)
+                :use_key('}'),
+            Rule('[ ', ' ]')
+                :with_pair(function() return false end)
+                :with_move(function(opts)
+                    return opts.prev_char:match('.%]') ~= nil
+                end)
+                :use_key(']')
+        })
     end,
 }
