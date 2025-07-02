d22bf0ca5e6d1d7dacd349f344f56a7ef61990a5
bufferOverflow06 <sujanjacharya123@gmail.com>
2025-07-02 10:56:18 +0530

    Updated neo-tree.lua plugin     - Added choice for deleting node

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,3 +1,18 @@
+-- return {
+--     "nvim-neo-tree/neo-tree.nvim",
+--     dependencies = {
+--         "nvim-tree/nvim-web-devicons",
+--         "nvim-lua/plenary.nvim",
+--         "MunifTanjim/nui.nvim",
+--     },
+--
+--     -- Nvim-tree keymaps
+--     config = function()
+--         vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+--         vim.keymap.set('n', '<C-j>', ':Neotree toggle<CR>', {})
+--     end
+-- }
+--
 return {
     "nvim-neo-tree/neo-tree.nvim",
     dependencies = {
@@ -6,9 +21,30 @@ return {
         "MunifTanjim/nui.nvim",
     },
 
-    -- Nvim-tree keymaps
     config = function()
+        -- Keymaps
         vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
         vim.keymap.set('n', '<C-j>', ':Neotree toggle<CR>', {})
+
+        -- Setup Neo-tree with trash delete
+        require("neo-tree").setup({
+            filesystem = {
+                window = {
+                    mappings = {
+                        ["d"] = "delete",  -- Keep 'd' for delete, redefine behavior below
+                    },
+                },
+                commands = {
+                    delete = function(state)
+                        local node = state.tree:get_node()
+                        local path = node.path
+                        -- Use trash-cli's trash-put
+                        vim.fn.system({ "trash-put", path })
+                        vim.notify("Moved to Trash: " .. path)
+                        require("neo-tree.sources.manager").refresh(state.name)
+                    end,
+                },
+            },
+        })
     end
 }
